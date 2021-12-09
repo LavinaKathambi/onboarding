4332aaba7201ff18d2cfac18adbf70e2cffaef02
lavinakathambi <lavinakathambi.m@gmail.com>
2021-12-09 15:28:59 +0200

    first

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:onboarding/widgets/animated_indicator.dart';
+import 'package:onboarding/widgets/onboard.dart';
+
+const kTitleStyle = TextStyle(
+    fontSize: 30, color: Color(0xFF01002f), fontWeight: FontWeight.bold);
+const kSubtitleStyle = TextStyle(fontSize: 22, color: Color(0xFF88869f));
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Onboarding Screens',
+      home: MainPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
